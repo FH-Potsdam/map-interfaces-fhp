573708de486778e7338d5118332f7b504fc07f74
Marcus Paeschke <marcus@mphasize.de>
2012-10-29 22:04:13 +0100

    Working Example for GeoJSON, just missing interaction

--- examples/leaflet-geojson.js
+++ examples/leaflet-geojson.js
@@ -10,29 +10,47 @@ var cartodb_apikey = "f909b68fa06f5aad09cddb976eeda7eddedee8a6";
 
 
 /**
- * Step 1: Zuerst muss Leaflet initialisiert werden, der Einfachheit halber speichern wir das Objekt in der Variable "map"
- * 
- * L.map('container-id', { options }) erzeugt ein neues Kartenobjekt und injiziert es in das DOM-Element mit der ID "container-id", 
- * bzw. im Beispiel in unseren DIV-Container mit der ID "map"
- * 
- * Optional kann man den Kartenausschnitt auch später mit .setView verändern.
- * .setView([x,y], zoom) zentriert unsere neue Karte an den angegebenen X-Y-Koordinaten mit der Zoomstufe 13
+ * Step 1: Konfiguration unserer Karten-Layer
+ */
+var layer = {
+	basemap1 : L.tileLayer('http://{s}.tile.cloudmade.com/5c5f709891f240bbba32d5f42f1926ec/997/256/{z}/{x}/{y}.png', {
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+	}),
+	basemap2 : L.tileLayer('http://{s}.tile.cloudmade.com/5c5f709891f240bbba32d5f42f1926ec/999/256/{z}/{x}/{y}.png', {
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+	}),
+	tiles : L.tileLayer('http://marcus.maps-fhp.de:8181/tiles/fhpinfrastructure/{z}/{x}/{y}.png?map_key=' + cartodb_apikey),
+	geojson : L.geoJson(),
+	geojsonInteractive : L.geoJson()
+}
+
+layer.config = {
+	base : {
+		"Roadmap" : layer.basemap1,
+		"Midnight" : layer.basemap2			
+	},
+	overlay : {
+		"Simple GeoJSON" : layer.geojson,
+		"Interactive GeoJSON": layer.geojsonInteractive,
+		"Tiles" : layer.tiles
+	}	
+}
+
+
+
+/**
+ * Step 2: Zuerst muss Leaflet initialisiert werden, der Einfachheit halber speichern wir das Objekt in der Variable "map"
  */
 var map = L.map('map', {
 	center : [52.4125, 13.051],
-	zoom : 17
+	zoom : 17,
+	layers : [layer.basemap1, layer.geojson]
 });
 
-
-var layer = {
-	basemap : L.tileLayer('http://{s}.tile.cloudmade.com/5c5f709891f240bbba32d5f42f1926ec/997/256/{z}/{x}/{y}.png', {
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
-	}),
-	geojson : L.geoJson()
-}
-
-layer.basemap.addTo(map);
-layer.geojson.addTo(map);
+/**
+ * Step 3: LayerSwitch
+ */
+var layerswitch = L.control.layers(layer.config.base, layer.config.overlay).addTo(map);
 
 
 /**
@@ -43,5 +61,6 @@ $(document).ready(function () {
 	var query = 'SELECT * FROM fhpinfrastructure';
 	$.getJSON('http://marcus.maps-fhp.de:8080/api/v1/sql?format=GeoJSON&q=' + encodeURIComponent(query), function (data) {
 		layer.geojson.addData(data);
+		layer.geojsonInteractive.addData(data);
 	});	
 });

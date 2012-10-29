7f7397b0b6e4282d3a7691be38cf776e12e1924f
Sebastian Meier <contact@sebastianmeier.eu>
2012-10-29 23:40:26 +0100

    we don't need wax :)

--- examples/leaflet-tilelayer.js
+++ examples/leaflet-tilelayer.js
@@ -70,7 +70,8 @@ var ste = new L.StamenTileLayer("terrain");
 var stw = new L.StamenTileLayer("watercolor");
 
 //TileMill via MapBox
-var tml = new wax.leaf.connector({tiles:['http://a.tiles.mapbox.com/v3/juli84.map-xf9ni6a1.jsonp']});
+
+var osm = new L.TileLayer('http://a.tiles.mapbox.com/v3/juli84.map-xf9ni6a1/{z}/{x}/{y}.png');
 
 /**
  * Die einzelnen Layer werden in Variablen gespeichert und später über die "Control" automatisch
